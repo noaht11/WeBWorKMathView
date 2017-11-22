8a1897266e02c2956667f3991e6bf95516cc8ee1
Noah Tajwar <noah.tajwar@gmail.com>
2017-11-22 00:09:05 -0800

    Switched to use KaTeX to render by default, and only fall back on MathJax

--- src/content.js
+++ src/content.js
@@ -12,15 +12,23 @@ injectScript(function()
 
     var hideOutput = function(element) {element.style.display = "none";};
     var showOutput = function(element) {element.style.display = "inline-block";};
-    var updateText = function(element, amath) {element.textContent = "`" + amath + "`";};
+    var updateText = function(element, amath) {element.textContent = "`" + amath +  "`";};
 
     window.UpdateMath = function(i, amath)
     {
         var outA = getOutput(i, "a");
         var outB = getOutput(i, "b");
 
-        MathJax.Hub.Queue([hideOutput, outB], [updateText, outB, amath], ["Typeset", MathJax.Hub, outB], [showOutput, outB],
+        try
+        {
+            var texstring = AMTparseMath(amath);
+            katex.render(texstring, outA);
+        }
+        catch(err)
+        {
+            MathJax.Hub.Queue([hideOutput, outB], [updateText, outB, amath], ["Typeset", MathJax.Hub, outB], [showOutput, outB],
                           [hideOutput, outA], [updateText, outA, amath], ["Typeset", MathJax.Hub, outA], [showOutput, outA], [hideOutput, outB]);
+        }
     }
 });
 
@@ -32,7 +40,7 @@ function createMathJaxOutDiv(i, subIndex, amath)
    theMathJaxOut.style.padding = "8px";
    theMathJaxOut.style.color = "#000000";
    theMathJaxOut.style.backgroundColor = "#dddddd";
-   theMathJaxOut.textContent = "`" + amath + "`";
+   theMathJaxOut.textContent = amath;
 
    return theMathJaxOut;
 }
@@ -57,4 +65,23 @@ for(var i = 0; i < inputs.length; i++)
    theMathJaxOut.appendChild(theMathJaxOutB);
    
    theInput.parentNode.insertBefore(theMathJaxOut, theInput.nextSibling);
+
+   try
+   {
+       var texstring = AMTparseMath(theInput.value);
+       katex.render(texstring, theMathJaxOutA);
+   }
+   catch(err)
+   {
+       theMathJaxOutA.textContent = "`" + theInput.value + "`";        
+       theMathJaxOutB.textContent = "`" + theInput.value + "`";
+   }
 }
+
+var script = document.createElement("script");
+script.src = chrome.runtime.getURL("katex/katex.min.js");
+document.head.appendChild(script);
+
+var script = document.createElement("script");
+script.src = chrome.runtime.getURL("ASCIIMathTeXImg.js");
+document.head.appendChild(script);
