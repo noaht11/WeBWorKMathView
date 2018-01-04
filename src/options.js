3ce8e1fa17ec4ec8c2e8fdbaff483ae9d6835a1b
Noah Tajwar <noah.tajwar@gmail.com>
2018-01-03 18:18:28 -0800

    Almost works

--- src/options.js
+++ src/options.js
@@ -1,6 +1,6 @@
-ExtConfig.Storage.setData(new ExtConfig.Storage.Data(false, ["webwork.elearning.ubc.ca"], false), function () {
+/*ExtConfig.Storage.setData(new ExtConfig.Storage.Data(false, ["webwork.elearning.ubc.ca"], false), function () {
     //console.log("Saved");
-});
+});*/
 
 /**
  * Handles click events on the checkbox button
@@ -23,20 +23,26 @@ function loadData() {
             disableManual();
         }
 
-        var wwHostInputs = getWWHostInputs();
-        for (var i = 0; i < wwHostInputs.length; i++) {
-            var theHost = data.wwHosts[i];
-            if (theHost) {
-                wwHostInputs[i].value = theHost;
-            }
+        // Clear all current ww host elements
+        var wwHostElements = getWWHostElements();
+        var wwHostsContainer = document.getElementById("wwHosts");
+        for(var i = 0; i < wwHostElements.length; i++) {
+            wwHostsContainer.removeChild(wwHostElements[i].parentNode);
+        }
+
+        for (var j = 0; j < data.wwHosts.length; j++) {
+            var theHost = data.wwHosts[j];
+            addWWHostElement(theHost);
         }
     });
 }
 
-function saveData() {
-    var data = collectData();
+function disableManual() {
+    // TODO 
+}
+
+function saveData(data) {
     ExtConfig.Storage.setData(data, function () {
-        console.log(data);
         loadData();
     });
 }
@@ -46,16 +52,93 @@ function collectData() {
     var enableWolfram = false;
     var wwHosts = [];
 
-    var wwHostInputs = getWWHostInputs();
-    for (var i = 0; i < wwHostInputs.length; i++) {
-        var inputText = wwHostInputs[i].value;
-        var theWWHost = extractHostname(inputText);
-        if (theWWHost) {
-            wwHosts.push(theWWHost);
-        }
+    var wwHostElements = getWWHostElements();
+    for (var i = 0; i < wwHostElements.length; i++) {
+        var wwHost = wwHostElements[i].textContent;
+        wwHosts.push(wwHost);
     }
 
     return new ExtConfig.Storage.Data(autoDetectWW, wwHosts, enableWolfram);
+}
+
+function getWWHostElements() {
+    return document.getElementsByClassName("wwHostText");
+}
+
+function addWWHostElement(text) {
+    var container = document.createElement("div");
+    container.className = "wwHost";
+
+    var textElement = document.createElement("div");
+    textElement.className = "wwHostText";
+    textElement.textContent = text;
+
+    var deleteButton = document.createElement("div");
+    deleteButton.className = "deleteButton";
+    deleteButton.innerHTML = "&times;";
+    deleteButton.addEventListener("click", function (event) {
+        document.getElementById("wwHosts").removeChild(this.parentNode);
+
+        ExtConfig.Storage.getData(function (data) {
+            data.wwHosts.splice(data.wwHosts.indexOf(text), 1);
+            
+            ExtConfig.Permissions.updatePermissions(data, function (granted) {
+                ExtConfig.Events.registerOnPageChangedRules(data);
+
+                saveData(data);
+            });
+        });
+    });
+
+    container.appendChild(textElement);
+    container.appendChild(deleteButton);
+
+    var parent = document.getElementById("wwHosts");
+    parent.insertBefore(container, document.getElementById("addButton"));
+}
+
+function showWWHostInputDialog() {
+    document.getElementById("wwHostInputDialog").style.display = "block";
+    document.getElementById("wwHostInputField").focus();
+}
+
+function hideWWHostInputDialog() {
+    document.getElementById("wwHostInputDialog").style.display = "none";
+    document.getElementById("wwHostInputField").value = "";
+    document.getElementById("wwHostInputMsg").textContent = "";
+}
+
+function validateWWHost() {
+    var text = document.getElementById("wwHostInputField").value;
+    var hostname = extractHostname(text);
+
+    var msgElement = document.getElementById("wwHostInputMsg");
+
+    if(hostname) {
+        // Retrieve the current data
+        ExtConfig.Storage.getData(function (data) {
+            // Add the hostname we want permission for
+            data.wwHosts.push(hostname);
+            // Request the new permissions
+            console.log("Pre-permissions:");
+            console.log(data);
+            ExtConfig.Permissions.updatePermissions(data, function (granted) {
+                if(granted) {
+                    // Permission granted! Register rules and save the data
+                    ExtConfig.Events.registerOnPageChangedRules(data);
+                    saveData(data);
+                    hideWWHostInputDialog(); 
+                }
+                else {
+                    // Permission denied :( Show an error
+                    msgElement.textContent = "You must grant the permission to use the extension on this WeBWorK site";
+                }
+            });
+        });
+    }
+    else {
+        msgElement.textContent = "Invalid URL / Hostname";
+    }
 }
 
 /**
@@ -86,167 +169,10 @@ function extractHostname(text) {
     return null;
 }
 
-function validateWWHostInputs() {
-    var wwHostInputs = getWWHostInputs();
-    for (var i = 0; i < wwHostInputs.length; i++) {
-        var theWWHost = wwHostInputs[i].value;
-
-        if (theWWHost) {
-            var url;
-            try {
-                url = new URL(theWWHost);
-            }
-            catch (err) {
-
-            }
-        }
-    }
-}
-
-function getWWHostInputs() {
-    var wwHostInputs = [];
-    for (var i = 0; i < 3; i++) {
-        wwHostInputs.push(document.getElementById("wwHostInput" + i));
-    }
-    return wwHostInputs;
-}
-
-function disableManual() {
-    // TODO 
-}
-
-// Saves options to chrome.storage
-function save_options() {
-    var status = document.getElementById("status");
-    var urlString = document.getElementById("urlIn").value;
-
-    var errorColor = "#ff0000";
-    var successColor = "#00bb44";
-
-    var url;
-    try {
-        url = new URL(urlString);
-    }
-    catch (err) {
-        console.log(err);
-        status.textContent = "Invalid URL";
-        status.style.color = errorColor;
-        return;
-    }
-
-    var hostname = url.hostname;
-    var permissionTarget = "*://" + hostname + "/*";
-
-    // Permissions must be requested from inside a user gesture, like a button's
-    // click handler.
-    chrome.permissions.request({
-        origins: [permissionTarget]
-    }, function (granted) {
-        // The callback argument will be true if the user granted the permissions.
-        if (granted) {
-            chrome.storage.sync.set(
-                {
-                    webworkHostname: hostname
-                },
-                function () {
-                    // Listen for events on the new hostname
-                    registerRules();
-
-                    // Update status to let user know options were saved.
-                    status.textContent = "Saved";
-                    status.style.color = successColor;
-
-                    // Update the view mode
-                    restore_options();
-                    saveSuccess();
-
-                    // Remove the status message after a short delay
-                    setTimeout(function () {
-                        status.textContent = "";
-                    }, 750);
-                });
-        } else {
-            status.textContent = "You must click \"Allow\" on the permission popup";
-            status.style.color = errorColor;
-        }
-    });
-}
-
-function restore_options() {
-    chrome.storage.sync.get(
-        {
-            webworkHostname: ""
-        },
-        function (items) {
-            var contentText = document.getElementById("currentDataContentText");
-            if (items.webworkHostname.length > 0) {
-                contentText.textContent = items.webworkHostname;
-                console.log(items.webworkHostname);
-                document.getElementById("urlIn").value = ("https://" + items.webworkHostname);
-                contentText.style.color = "#ffffff";
-            }
-            else {
-                contentText.textContent = "Not Set";
-                contentText.style.color = "#b6b6b6";
-            }
-        });
-}
-
-function editData() {
-    document.getElementById("currentDataContentText").style.display = "none";
-    var inputField = document.getElementById("dataInputField");
-    inputField.style.display = "block";
-
-    document.getElementById("editButton").style.display = "none";
-    document.getElementById("saveButton").style.display = "block";
-
-    var urlIn = document.getElementById("urlIn");
-    urlIn.focus();
-    urlIn.select();
-}
-
-/*function saveData() {
-    save_options();
-}*/
-
-function saveSuccess() {
-    document.getElementById("currentDataContentText").style.display = "block";
-    var inputField = document.getElementById("dataInputField");
-    document.getElementById("dataInputField").style.display = "none";
-
-    document.getElementById("editButton").style.display = "block";
-    document.getElementById("saveButton").style.display = "none";
-}
-
-function allURLsPermission() {
-
-    console.log("Permission click");
-
-    ExtConfig.Storage.getData(function (data) {
-        console.log("Loaded");
-        console.log(data);
-
-        ExtConfig.Permissions.updatePermissions(data, function (granted) {
-            console.log("Permissions done");
-            console.log(granted);
-
-            if (granted) {
-                ExtConfig.Events.registerOnPageChangedRules(data);
-            }
-        })
-    });
-
-    /*chrome.permissions.request({
-        origins: ["<all_urls>"]
-    }, function (granted) {
-        console.log("granted");
-    });*/
-}
-
 document.getElementById("autoDetectContainer").addEventListener("click", toggleAutoDetect);
-document.getElementById("saveButton").addEventListener("click", saveData);
 
-//document.getElementById("editButton").addEventListener("click", editData);
-//document.getElementById("saveButton").addEventListener("click", saveData);
+document.getElementById("addButton").addEventListener("click", showWWHostInputDialog);
+document.getElementById("wwHostInputClose").addEventListener("click", hideWWHostInputDialog);
+document.getElementById("wwHostInputSave").addEventListener("click", validateWWHost);
 
 document.addEventListener("DOMContentLoaded", loadData);
